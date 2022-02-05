bcf3df56e4cc802bd19e9a099ab2abeee85249d9
luka <me@luka-sj.com>
2022-02-05 22:27:16 +0100

    Add new commands

---
+++ src/app/commands/pry.rb
@@ -0,0 +1,22 @@
+#===============================================================================
+#  Pry terminal output command
+#===============================================================================
+class CommandPry < Commands::BaseCommand
+  #  command metadata
+  name        'pry'
+  version     '1.0.0'
+  description 'Run interactive Ruby session to debug current context'
+
+  register
+  #-----------------------------------------------------------------------------
+  #  process command action
+  #-----------------------------------------------------------------------------
+  def process
+    #  pry called from CommandPry#process
+    #  opened a new interactive Ruby session
+    #  from here, you have access to all system/environment variables or modules
+    #  when done with the session, run the command `exit` to close the session
+    binding.pry
+  end
+  #-----------------------------------------------------------------------------
+end
---
+++ src/app/commands/reload.rb
@@ -0,0 +1,42 @@
+#===============================================================================
+#  Reload terminal output command
+#===============================================================================
+class CommandReload < Commands::BaseCommand
+  #  command metadata
+  name        'reload'
+  version     '1.0.0'
+  description 'Reloads all scripts running in context'
+
+  register
+  #-----------------------------------------------------------------------------
+  #  process command action
+  #-----------------------------------------------------------------------------
+  def process
+    #  clear console
+    Commands::Handler.try('clear')
+    Console.setup
+    Console.echo_p('Starting full system reload ...', 2)
+    #  reload core scripts
+    Env::CORE_DIRECTORIES.each do |f|
+      load "#{Env.initial_directory}/#{f}"
+      Console.echo_li("reloaded script '#{f}'")
+      Console.echo_p
+    end
+    #  reload gems
+    Console.echo_p
+    Core::Gemfile.install
+    Core::Gemfile.load
+    Env.run_before_init
+    #  reload application scripts
+    Env::APP_DIRECTORIES.each do |f|
+      load "#{Env.initial_directory}/#{f}"
+      Console.echo_li("reloaded script '#{f}'")
+      Console.echo_p
+    end
+    Env.run_after_init
+    #  print final message
+    Console.echo_p
+    Console.echo_p("System reload $successful$.", 2)
+  end
+  #-----------------------------------------------------------------------------
+end
