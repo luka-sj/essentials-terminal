e9f40144f8cd569f6843315c3d155219cc86fd1c
luka <me@luka-sj.com>
2022-02-11 10:57:52 +0100

    Update system config

--- src/config/gems.rb
+++ src/config/gems.rb
@@ -2,6 +2,7 @@
 #  Configure required gems for console application
 #===============================================================================
 class Core::Gemfile
+  gem 'net-ssh', 'net/ssh'
   gem 'pry'
   gem 'rouge'
   gem 'rubyzip', 'zip'
